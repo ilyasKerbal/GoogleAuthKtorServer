d5646aa5482bc2f7c30972ca1aa12f16cc51aed1
ILYAS KERBAL <kerbalsc@gmail.com>
2022-07-30 14:53:31 +0100

    Starter project generated using start.ktor.io

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+}
+
+group = "io.github.ilyaskerbal"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    // Content negotiation
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+
+    // Sessions and Auth
+    implementation("io.ktor:ktor-server-sessions-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+
+}
---
+++ src/main/kotlin/io/github/ilyaskerbal/Application.kt
@@ -0,0 +1,15 @@
+package io.github.ilyaskerbal
+
+import io.ktor.server.application.*
+import io.github.ilyaskerbal.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
